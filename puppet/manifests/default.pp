4425013db515662d41cdd4b47f5f51b38d1e293f
Toby Jackson <admin@warmfusion.co.uk>
2015-02-07 12:41:39 +0000

    Workaround for a little bug in sensu puppet and shore up some checks

--- puppet/manifests/default.pp
+++ puppet/manifests/default.pp
@@ -21,6 +21,11 @@ node default inherits basenode {
   package { 'wget':
     ensure => installed,
   }
+  
+  # Required for sensu-plugins to install
+  package {'ruby-dev':
+    ensure => installed,
+  }-> Package['sensu-plugin']
 
   file { '/opt/sensu-plugins':
     ensure => directory,
@@ -31,27 +36,25 @@ node default inherits basenode {
     source => 'https://github.com/sensu/sensu-community-plugins/archive/master.tar.gz',
     target => '/opt/sensu-plugins',
     require => File['/opt/sensu-plugins'],
-  }
-  sensu::handler { 'default':
-    command => 'mail -s \'sensu alert\' ops@foo.com',
-  }
-  sensu::check { 'check_cron':
-    command => '/opt/sensu-plugins/sensu-community-plugins-master/plugins/processes/check-procs.rb -p crond -C   1',
-    handlers => 'default',
-    subscribers => 'base',
-    require => Staging::Deploy['sensu-community-plugins.tar.gz'],
-  }
-  sensu::check { 'check_dns':
-    command => '/opt/sensu-plugins/sensu-community-plugins-master/plugins/dns/check-dns.rb -d google-public-dns-a.google.com -s 192.168.1.2 -r 8.8.8.8',
-    handlers => 'default',
-    subscribers => 'base',
-    require => Staging::Deploy['sensu-community-plugins.tar.gz'],
-  }
-  sensu::check { 'check_disk':
-    command => '/opt/sensu-plugins/sensu-community-plugins-master/plugins/system/check-disk.rb',
-    handlers => 'default',
-    subscribers => 'base',
-    require => Staging::Deploy['sensu-community-plugins.tar.gz'],
-  }
+  } 
 
+#  sensu::handler { 'default':
+#    command => 'mail -s \'sensu alert\' ops@foo.com',
+#  }
+
+  sensu::check { 'check_disk':
+    command => '/opt/sensu-plugins/sensu-community-plugins-master/plugins/system/disk-metrics.rb',
+    handlers => 'default',
+    subscribers => 'base',
+  }
+  sensu::check { 'check_cpu':
+    command => '/opt/sensu-plugins/sensu-community-plugins-master/plugins/system/check-cpu.rb',
+    handlers => 'default',
+    subscribers => 'base',
+  }
+  sensu::check { 'check_load':
+    command => '/opt/sensu-plugins/sensu-community-plugins-master/plugins/system/check-load.rb',
+    handlers => 'default',
+    subscribers => 'base',
+  }
 }
